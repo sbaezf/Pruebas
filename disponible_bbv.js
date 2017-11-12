2cfa141c30ad4787afffb38d7932e4245d6738a6
sbaezf <s.baezf@gmail.com>
2017-11-12 22:52:19 +0100

    Update disponible_bbv.js

--- disponible_bbv.js
+++ disponible_bbv.js
@@ -5,7 +5,7 @@ function generarResultado(){
     
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("txtHint").innerHTML = this.responseText;
+			alert(this.responseText);
 		}
     };
     
